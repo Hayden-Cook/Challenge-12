44b93045f7b1741c6c0431c67b1de1461a688260
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-11-24 13:46:36 -0600

    added manager_id to seeds

--- seeds.sql
+++ seeds.sql
@@ -10,8 +10,8 @@ INSERT INTO role (title, salary, department_id) VALUES
     ('HR Specialist', 60000, 3),
     ('Sales Lead', 120000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES
-    ('Ryan', 'Reynolds', 1),
-    ('Hugh', 'Jackman', 2),
-    ('Stephen', 'Amell', 3),
-    ('Robert', 'Downey', 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+    ('Ryan', 'Reynolds', 1, NULL),
+    ('Hugh', 'Jackman', 2, 1),
+    ('Stephen', 'Amell', 3, NULL),
+    ('Robert', 'Downey', 4, 3);
