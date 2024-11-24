a4d25ec3bb07b872d0a19b5d16e801770051ae84
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-11-24 15:13:17 -0600

    fixed another schema issue with deleting

--- schema.sql
+++ schema.sql
@@ -19,6 +19,6 @@ CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER REFERENCES role(id),
-    manager_id INTEGER REFERENCES employee(id)
+    role_id INTEGER REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INTEGER REFERENCES employee(id) ON DELETE SET NULL
 );
